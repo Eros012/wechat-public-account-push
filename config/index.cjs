2945954099f73f2deb47def3f971675543cd9403
Eros012 <104420431+Eros012@users.noreply.github.com>
2024-04-02 13:40:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -16,6 +16,53 @@ const USER_CONFIG = {
   PROVINCE: '四川',
   CITY: '成都',
 
+   // 功能开关,打开：true，关闭：false
+  SWITCH: {
+    /** 每日天气 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: true,
+
+    /** 节假日 */
+    // 下一休息日综合提醒, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    holidaytts: true,
+
+    /** 每日N句 */
+    // 金山每日一句, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    CIBA: true,
+    
+    // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    oneTalk: false,
+    
+    // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    earthyLoveWords: false,
+    
+    // 朋友圈文案, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    momentCopyrighting: false,
+    
+    // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poisonChickenSoup: false,
+    
+    // 古诗古文, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poetry: false,
+
+    /** 星座运势 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    horoscope: true,
+  
+    /** 生日消息和节日消息 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    birthdayMessage: true,
+  
+    /** 学生课表 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    courseSchedule: false,
+  },
+  
+
+  /** 是否给文字设置多彩颜色, 和emoji不兼容 */
+  // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
+  IS_SHOW_COLOR: false,
+  
   USERS: [
     {
       // 想要发送的人的名字
@@ -26,32 +73,35 @@ const USER_CONFIG = {
       useTemplateId: 'ymClpu-GDCzxQM75LZJvqSBbsra49TK6xUQAKPUxho8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-09',
+      horoscopeDateType: '今日',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
+       /** {
           type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
+        },*/
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: 'Eros', year: '2001', date: '05-04',
+        }
+        {
           type: '生日', name: '耶耶', year: '1997', date: '05-09',
         },
-        {
+       /** {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
+        },*/
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2024-04-02' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+       /** { keyword: 'marry_day', date: '2022-09-09' },*/
       ],
     },
   ],
 
 
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+ /** // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
@@ -60,7 +110,7 @@ const USER_CONFIG = {
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
     }
-  ],
+  ],*/
 
 }
 
